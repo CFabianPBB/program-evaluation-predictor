b6015de83f7d422f02225ecd44ce8e38e3973921
CFabianPBB <chris.fabian@tylertech.com>
2025-03-11 10:55:31 -0600

    resolve the "Not Found" issue

--- server.js
+++ server.js
@@ -30,11 +30,11 @@ app.use('/api', require('./routes/api'));
 
 // Serve static files from React build in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+  app.use(express.static(path.join(__dirname, './client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
 }
 
 // Define port
