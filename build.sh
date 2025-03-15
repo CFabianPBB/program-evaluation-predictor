c37b81a91e1cb1aca4f3ed3f2c5d1a43a048a83c
CFabianPBB <chris.fabian@tylertech.com>
2025-03-14 21:29:07 -0600

    Fix build script recursion

--- build.sh
+++ build.sh
@@ -2,8 +2,17 @@
 # Exit on error
 set -o errexit
 
+# Install dependencies
 npm install
+
+# Go to client directory
 cd client
+
+# Install client dependencies
 npm install
+
+# Build the client
 npm run build
+
+# Return to root directory
 cd ..
